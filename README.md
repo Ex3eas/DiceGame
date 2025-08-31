44cd34457c53b938ae03c4eacd81256ec10f79c2
Ex3eas <muhammadeesaaslam@gmail.com>
2025-08-31 15:22:17 +0100

    Updates to the game
    Added multiple buttons (Reset, Stop) and also added some text (Rules which are below). Made the stop button for simplicity rather than having multiple rounds

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# DiceGame
+# DiceGame 
+
+
+
+1\. 2 dices per player 
+
+2\. 5 Rounds
+
+3\. If a player gets an even number then their answer is +10 
+
+4\. If a player gets an odd number on both dices their answer is -5
+
+5\. Once all 5 rounds are done the one with the most score wins!
+
--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,10 @@ let playerOneScore = 0;
 let playerTwoName = "";
 let playerTwoScore = 0;
 
-function generateNumberforP1() {
-  let randnum = Math.floor(Math.random() * 6 + 1);
-  if (randnum % 2 == 0) {
-    playerOneScore += 10;
-  } else {
-    playerOneScore -= 5;
-  }
-  playerOneScore += randnum;
-  playerOneScore = Math.max(playerOneScore, 0);
-  console.log(playerOneName + " rolled " + randnum);
-  console.log(playerOneName + "score is " + playerOneScore);
-  const playerOneScoreDiv = document.getElementById("p1score");
-  playerOneScoreDiv.innerHTML =
-    playerOneName + " 's score is " + playerOneScore;
+function end() {
+  const btn = document.getElementById("play");
+  btn.disabled = true;
+  console.log(" The Game has been Stopped ");
 }
 
 function generateNumber(player) {
@@ -49,22 +39,6 @@ function generateNumber(player) {
     playerTwoScoreDiv.innerHTML =
       playerTwoName + " 's score is " + playerTwoScore;
   }
-}
-
-function generateNumberforP2() {
-  let randnum2 = Math.floor(Math.random() * 6 + 1);
-  if (randnum2 % 2 == 0) {
-    playerTwoScore += 10;
-  } else {
-    playerTwoScore -= 5;
-  }
-  playerTwoScore += randnum2;
-  playerTwoScore = Math.max(playerTwoScore, 0);
-  console.log(playerTwoName + " rolled " + randnum2);
-  console.log(playerTwoName + " score is " + playerTwoScore);
-  const playerTwoScoreDiv = document.getElementById("p2score");
-  playerTwoScoreDiv.innerHTML =
-    playerTwoName + " 's score is " + playerTwoScore;
 }
 
 function storeName() {
